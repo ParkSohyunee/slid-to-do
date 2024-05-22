40bab32f97aef8b00158685f9dfff36be0ecd8ef
ParkSohyunee <sohyunee016@gmail.com>
2024-05-23 00:17:41 +0900

    style: font size 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,13 @@ export default function Home() {
       <div className="font-medium">Medium</div>
       <div className="font-normal">Normal</div>
       <div className="font-light">Light</div>
+      <div className="text-3xl">3xl</div>
+      <div className="text-2xl">2xl</div>
+      <div className="text-xl">xl</div>
+      <div className="text-lg">lg</div>
+      <div className="text-base">base</div>
+      <div className="text-sm">sm</div>
+      <div className="text-xs">xs</div>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,10 +11,14 @@ const config: Config = {
       fontFamily: {
         pretendard: ["Pretendard"],
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        xs: ["12px", "16px"],
+        sm: ["14px", "20px"],
+        base: ["16px", "24px"],
+        lg: ["18px", "28px"],
+        xl: ["20px", "28px"],
+        "2xl": ["24px", "32px"],
+        "3xl": ["30px", "36px"],
       },
     },
   },
