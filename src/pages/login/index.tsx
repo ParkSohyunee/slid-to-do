1a710827250dc6d8736bf8844c06eec5d643de50
ParkSohyunee <sohyunee016@gmail.com>
2024-05-24 00:30:38 +0900

    design: 로그인 페이지 레이아웃 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { useForm } from "react-hook-form"
 import { AxiosError } from "axios"
 
@@ -46,17 +47,34 @@ export default function LoginPage() {
   }
 
   return (
-    <form onSubmit={handleSubmit(handleSubmitForm)}>
-      <label htmlFor="email">아이디</label>
-      <input {...register("email", emailValidationRules)} autoComplete="off" />
-      <p>{errors.email?.message}</p>
-      <label htmlFor="password">비밀번호</label>
-      <input
-        type="password"
-        {...register("password", passwordValidationRules)}
-      />
-      <p>{errors.password?.message}</p>
-      <button type="submit">로그인하기</button>
-    </form>
+    <section className="flex flex-col items-center mt-[120px]">
+      <div className="mb-10">
+        <Image src={"/logo/img_logo.svg"} alt="logo" width={270} height={89} />
+      </div>
+      <form
+        onSubmit={handleSubmit(handleSubmitForm)}
+        className="flex flex-col gap-12 max-w-screen-sm w-full"
+      >
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col">
+            <label htmlFor="email">아이디</label>
+            <input
+              {...register("email", emailValidationRules)}
+              autoComplete="off"
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              {...register("password", passwordValidationRules)}
+            />
+            <p>{errors.password?.message}</p>
+          </div>
+        </div>
+        <button type="submit">로그인하기</button>
+      </form>
+    </section>
   )
 }
