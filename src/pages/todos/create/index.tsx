0fe7fca15a1f083479e7914cb671dea7ee28457d
ParkSohyunee <sohyunee016@gmail.com>
2024-05-24 23:48:54 +0900

    design: 할 일 생성 버튼 및 페이지 레이아웃 구현

--- src/pages/todos/create/index.tsx
+++ src/pages/todos/create/index.tsx
@@ -20,7 +20,7 @@ export default function CreateTodosPage() {
   const [selectedOption, setSelectedOption] = useState<SelectedOption>("file")
   const {
     register,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<TodosFormVaules>({ mode: "onBlur" })
 
   const handleToggleSelect = (value: SelectedOption) => {
@@ -29,18 +29,20 @@ export default function CreateTodosPage() {
 
   return (
     <section>
-      <div>
-        <div className="relative text-field">
-          <div className="flex flex-col">
-            <label
-              htmlFor="title"
-              className="text-base font-semibold mb-3 text-basic"
-            >
-              아이디
-            </label>
-            <input
-              {...register("title", todosTitleValidationRules)}
-              className={`
+      <h2 className="text-lg font-bold text-basic mb-6">할 일 생성</h2>
+      <form className="flex flex-col gap-10">
+        <div className="flex flex-col gap-6">
+          <div className="relative text-field">
+            <div className="flex flex-col">
+              <label
+                htmlFor="title"
+                className="text-base font-semibold mb-3 text-basic"
+              >
+                제목
+              </label>
+              <input
+                {...register("title", todosTitleValidationRules)}
+                className={`
               px-6 py-3 
               rounded-sm 
               bg-slate-50 
@@ -52,37 +54,38 @@ export default function CreateTodosPage() {
                   : "border-slate-50 hover:border-blue-300"
               }
               `}
-              type="text"
-              autoComplete="off"
-              placeholder="이메일을 입력해 주세요"
-            />
+                type="text"
+                autoComplete="off"
+                placeholder="할 일의 제목을 적어주세요"
+              />
+            </div>
+            <p className="text-sm font-normal text-error mx-4 mt-2">
+              {errors.title?.message}
+            </p>
           </div>
-          <p className="text-sm font-normal text-error mx-4 mt-2">
-            {errors.title?.message}
-          </p>
-        </div>
-        <div>
-          <label
-            htmlFor="reference"
-            className="text-base font-semibold mb-3 text-basic"
-          >
-            자료
-          </label>
-          <div className="flex gap-3">
-            <CheckBoxButton
-              option="file"
-              selectedOption={selectedOption}
-              handleToggleSelect={handleToggleSelect}
-            />
-            <CheckBoxButton
-              option="link"
-              selectedOption={selectedOption}
-              handleToggleSelect={handleToggleSelect}
-            />
-          </div>
-          <input
-            {...register("linkUrl", todosLinkUrlValidationRules)}
-            className={`
+          <div className="relative text-field">
+            <div className="flex flex-col">
+              <label
+                htmlFor="reference"
+                className="text-base font-semibold mb-3 text-basic"
+              >
+                자료
+              </label>
+              <div className="flex gap-3 mb-3">
+                <CheckBoxButton
+                  option="file"
+                  selectedOption={selectedOption}
+                  handleToggleSelect={handleToggleSelect}
+                />
+                <CheckBoxButton
+                  option="link"
+                  selectedOption={selectedOption}
+                  handleToggleSelect={handleToggleSelect}
+                />
+              </div>
+              <input
+                {...register("linkUrl", todosLinkUrlValidationRules)}
+                className={`
               px-6 py-3 
               rounded-sm 
               bg-slate-50 
@@ -94,15 +97,32 @@ export default function CreateTodosPage() {
                   : "border-slate-50 hover:border-blue-300"
               }
               `}
-            type="text"
-            autoComplete="off"
-            placeholder="영상이나 글, 파일의 링크를 넣어주세요"
-          />
-          <p className="text-sm font-normal text-error mx-4 mt-2">
-            {errors.linkUrl?.message}
-          </p>
+                type="text"
+                autoComplete="off"
+                placeholder="영상이나 글, 파일의 링크를 넣어주세요"
+              />
+            </div>
+            <p className="text-sm font-normal text-error mx-4 mt-2">
+              {errors.linkUrl?.message}
+            </p>
+          </div>
         </div>
-      </div>
+        <button
+          type="submit"
+          className={`
+            py-3 flex 
+            justify-center items-center 
+            self-stretch rounded-sm 
+            text-white text-base font-semibold
+            outline-none
+            hover:bg-blue-600 active:bg-blue-800
+            ${isValid ? "bg-blue-500" : "bg-slate-400"}
+            transition-colors duration-500
+            `}
+        >
+          확인
+        </button>
+      </form>
     </section>
   )
 }
