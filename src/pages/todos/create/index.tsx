9b7758d0037022e6c06217133a1930ebfb075a9e
ParkSohyunee <sohyunee016@gmail.com>
2024-05-25 09:15:00 +0900

    feat: form input을 Label, TextField 컴포넌트로 분리

--- src/pages/todos/create/index.tsx
+++ src/pages/todos/create/index.tsx
@@ -1,67 +1,50 @@
 import { useState } from "react"
-import { useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 
 import CheckBoxButton from "@/components/buttons/CheckBoxButton"
 
-import {
-  todosTitleValidationRules,
-  todosLinkUrlValidationRules,
-} from "@/libs/utils/formInputValidationRules"
-import { SelectedOption } from "@/types/todos"
+import { todosLinkUrlValidationRules } from "@/libs/utils/formInputValidationRules"
+import { SelectedOption, TodosFormVaules } from "@/types/todos"
 
-type TodosFormVaules = {
-  title: string
-  fileUrl: "string"
-  linkUrl: "string"
-  goalId: number
-}
+import TextField from "@/components/TextField"
+import Label from "@/components/Label"
 
 export default function CreateTodosPage() {
   const [selectedOption, setSelectedOption] = useState<SelectedOption>("file")
+  const methods = useForm({ mode: "onBlur" })
   const {
     register,
     formState: { errors, isValid },
-  } = useForm<TodosFormVaules>({ mode: "onBlur" })
+  } = useForm<TodosFormVaules>({
+    mode: "onBlur",
+    defaultValues: {
+      title: "",
+      fileUrl: "",
+      linkUrl: "",
+      goalId: 0,
+    },
+  })
 
   const handleToggleSelect = (value: SelectedOption) => {
     setSelectedOption(value)
   }
 
+  const onSubmit = (data: any) => {
+    // form 제출
+    console.log(data)
+  }
+
   return (
-    <section>
+    <FormProvider {...methods}>
       <h2 className="text-lg font-bold text-basic mb-6">할 일 생성</h2>
-      <form className="flex flex-col gap-10">
+      <form
+        onSubmit={methods.handleSubmit(onSubmit)}
+        className="flex flex-col gap-10"
+      >
         <div className="flex flex-col gap-6">
-          <div className="relative text-field">
-            <div className="flex flex-col">
-              <label
-                htmlFor="title"
-                className="text-base font-semibold mb-3 text-basic"
-              >
-                제목
-              </label>
-              <input
-                {...register("title", todosTitleValidationRules)}
-                className={`
-              px-6 py-3 
-              rounded-sm 
-              bg-slate-50 
-              text-base font-normal text-basic placeholder:text-slate-400
-              border focus:border-blue-500 focus:outline-none
-              ${
-                errors.title
-                  ? "border-error hover:border-error"
-                  : "border-slate-50 hover:border-blue-300"
-              }
-              `}
-                type="text"
-                autoComplete="off"
-                placeholder="할 일의 제목을 적어주세요"
-              />
-            </div>
-            <p className="text-sm font-normal text-error mx-4 mt-2">
-              {errors.title?.message}
-            </p>
+          <div>
+            <Label htmlFor="title">제목</Label>
+            <TextField field="title" placeholder="할 일의 제목을 적어주세요" />
           </div>
           <div className="relative text-field">
             <div className="flex flex-col">
@@ -116,13 +99,12 @@ export default function CreateTodosPage() {
             text-white text-base font-semibold
             outline-none
             hover:bg-blue-600 active:bg-blue-800
-            ${isValid ? "bg-blue-500" : "bg-slate-400"}
             transition-colors duration-500
             `}
         >
           확인
         </button>
       </form>
-    </section>
+    </FormProvider>
   )
 }
--- src/types/todos.ts
+++ src/types/todos.ts
@@ -1,1 +1,8 @@
 export type SelectedOption = "file" | "link"
+
+export type TodosFormVaules = {
+  title: string
+  fileUrl?: string
+  linkUrl?: string
+  goalId: number
+}
