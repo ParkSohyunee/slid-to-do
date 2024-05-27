34b592fea2599b62d006e5cff67b367066597c1d
ParkSohyunee <sohyunee016@gmail.com>
2024-05-27 20:54:07 +0900

    design: 드롭다운 컴포넌트 스타일 적용

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { MouseEvent, useRef } from "react"
 import { useDropdownContext } from "@/context/DropdownContext"
 import { useDetectClose } from "@/hooks/useDetectClose"
@@ -5,9 +6,10 @@ import { GoalDetail } from "@/types/goal"
 
 type DropdownProps = {
   list: GoalDetail[]
+  defaultList: string
 }
 
-export default function Dropdown({ list }: DropdownProps) {
+export default function Dropdown({ list, defaultList }: DropdownProps) {
   const dropdownRef = useRef(null)
   const { selectedList, changeSelected } = useDropdownContext()
   const { isOpen, toggleHandler } = useDetectClose({ ref: dropdownRef })
@@ -20,26 +22,56 @@ export default function Dropdown({ list }: DropdownProps) {
 
   return (
     <div ref={dropdownRef}>
-      <input
-        onClick={toggleHandler}
-        defaultValue={selectedList}
-        readOnly
-        className={`
-        w-full px-6 py-3 
-        rounded-sm bg-slate-50 
-        text-base font-normal text-basic placeholder:text-slate-400
-        border focus:outline-none
-        border-slate-50 hover:border-blue-300
-        cursor-pointer
+      <div onClick={toggleHandler} className="relative">
+        <input
+          defaultValue={selectedList}
+          readOnly
+          className={`
+          w-full px-6 py-3 
+          rounded-sm bg-slate-50 
+          text-base font-normal text-basic placeholder:text-slate-400
+          border focus:outline-none
+          border-slate-50 hover:border-blue-300
+          cursor-pointer
         `}
-        type="text"
-        autoComplete="off"
-        placeholder="목표를 선택해주세요"
-      />
+          type="text"
+          autoComplete="off"
+          placeholder={defaultList}
+        />
+        <Image
+          className="absolute top-3 right-[20px] cursor-pointer"
+          src="/icons/arrow-down.svg"
+          alt="드롭다운 메뉴열기"
+          width={24}
+          height={24}
+        />
+      </div>
       {isOpen && (
-        <ul onClick={changeSelectedList}>
+        <ul
+          onClick={changeSelectedList}
+          className="rounded-sm bg-gray-50 relative top-[5px]"
+        >
+          {selectedList && (
+            <li
+              className={`
+              text-base font-normal text-slate-400 
+              rounded-sm py-[10px] px-4 cursor-default
+              `}
+            >
+              {defaultList}
+            </li>
+          )}
           {list?.map((list) => (
-            <li key={list.id} id={list.title}>
+            <li
+              key={list.id}
+              id={list.title}
+              className={`
+              text-base font-normal text-basic 
+              rounded-sm py-[10px] px-4
+              hover:bg-gray-200 cursor-pointer
+              ${selectedList === list.title ? "bg-blue-50" : "bg-gray-50"}
+              `}
+            >
               {list.title}
             </li>
           ))}
--- src/components/GoalDropdownForTodos.tsx
+++ src/components/GoalDropdownForTodos.tsx
@@ -5,6 +5,8 @@ import { DropdownProvider } from "@/context/DropdownContext"
 import { QUERY_KEYS } from "@/libs/constants/queryKeys"
 import getGoalList from "@/pages/api/goal/getGoalList"
 
+const DEFAUL_LIST = "목표를 선택해주세요"
+
 export default function GoalDropdownForTodos() {
   const { data } = useQuery({
     queryKey: [QUERY_KEYS.getGoalList],
@@ -13,7 +15,7 @@ export default function GoalDropdownForTodos() {
 
   return (
     <DropdownProvider>
-      <Dropdown list={data?.goals || []} />
+      <Dropdown list={data?.goals || []} defaultList={DEFAUL_LIST} />
     </DropdownProvider>
   )
 }
