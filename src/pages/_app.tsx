77ed3c23bf6f2986a9e0d06dd70ea87b20bd843e
ParkSohyunee <sohyunee016@gmail.com>
2024-05-28 11:08:11 +0900

    design: 로그인, 회원가입 페이지를 제외한 레이아웃 컴포넌트 전체페이지에 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { CookiesProvider } from "react-cookie"
+import Layout from "@/components/Layout"
 
 const queryClient = new QueryClient()
 
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <CookiesProvider>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </CookiesProvider>
     </QueryClientProvider>
   )
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,8 @@
-import Layout from "@/components/Layout"
-
 export default function DashboardPage() {
   return (
-    <Layout>
-      <div className="bg-white h-full max-w-1200">
-        <h1>대시보드</h1>
-        <div>내용</div>
-      </div>
-    </Layout>
+    <div className="bg-white h-full max-w-1200">
+      <h1>대시보드</h1>
+      <div>내용</div>
+    </div>
   )
 }
