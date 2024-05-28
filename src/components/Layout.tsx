053532bf6c829b8cfee73019098574f868e0d93f
ParkSohyunee <sohyunee016@gmail.com>
2024-05-28 10:31:52 +0900

    design: 반응형에 따른 사이드바 영역 레이아웃 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,8 +6,16 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <section className="flex h-dvh bg-slate-100">
-      <article className="bg-blue-200 xl:w-[280px] tablet:w-[60px] absolute h-full"></article>
+    <section className="flex h-dvh bg-slate-100 max-tablet:flex-col">
+      <article
+        className={`
+      bg-blue-200 
+      xl:w-[280px] tablet:w-[60px] 
+      absolute 
+      h-full 
+      max-tablet:relative 
+      max-tablet:h-12`}
+      ></article>
       <main className="w-full grow p-4 xl:pl-[360px] tablet:p-6 tablet:pl-[81px]">
         {children}
       </main>
