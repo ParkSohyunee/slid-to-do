e39412c1c901af66586698785776eeb9f0024131
ParkSohyunee <sohyunee016@gmail.com>
2024-05-28 15:42:45 +0900

    design: 컴포넌트 UI 구현 및 할 일 완료여부에 따른 스타일 적용

--- src/components/RecentlyTodosCard.tsx
+++ src/components/RecentlyTodosCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { useQuery } from "@tanstack/react-query"
 
 import { QUERY_KEYS } from "@/libs/constants/queryKeys"
@@ -9,7 +10,52 @@ export default function RecentlyTodosCard() {
     queryFn: getAllTodos,
   })
 
-  console.log(data)
+  console.log(data) // 삭제 예정
 
-  return <div className="bg-blue-300">최근 등록한 할 일</div>
+  return (
+    <div className="bg-white border border-slate-100 rounded-sm h-[250px] flex flex-col gap-4 px-6 pt-4 pb-6">
+      <div className="flex items-center justify-between">
+        <div className="flex justify-center items-center gap-2">
+          <Image
+            src="/icons/todo-recently.svg"
+            alt="최근 등록한 할 일"
+            width={40}
+            height={40}
+          />
+          <span className="text-lg font-semibold text-basic">
+            최근 등록한 할 일
+          </span>
+        </div>
+        <button className="flex items-center text-sm font-medium text-slate-600">
+          모두 보기
+          <Image
+            src="/icons/arrow-right.svg"
+            alt="모두 보기 버튼"
+            width={24}
+            height={24}
+          />
+        </button>
+      </div>
+      <ul className="pb-3 flex flex-col justify-between gap-2">
+        {data?.todos.map((todo) => (
+          <li
+            key={todo.id}
+            className={`flex gap-2 text-sm font-normal text-basic ${todo.done && "line-through"}`}
+          >
+            <Image
+              src={
+                todo.done
+                  ? "/icons/checkbox-active.svg"
+                  : "/icons/checkbox-inactive.svg"
+              }
+              alt="할 일 완료 여부"
+              width={24}
+              height={24}
+            />
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
