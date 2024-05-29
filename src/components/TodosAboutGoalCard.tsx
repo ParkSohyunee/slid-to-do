8671cf0a9e27d3e950133d8073ac09ac81d6cabd
ParkSohyunee <sohyunee016@gmail.com>
2024-05-29 16:49:37 +0900

    feat: 모든 목표 조회 및 목표별 할 일 조회 API 데이터 불러오는 작업

--- src/components/TodosAboutGoalCard.tsx
+++ src/components/TodosAboutGoalCard.tsx
@@ -1,6 +1,35 @@
 import Image from "next/image"
+import { useQuery } from "@tanstack/react-query"
+
+import getGoalList from "@/pages/api/goal/getGoalList"
+import getAllTodos from "@/pages/api/todos/getAllTodos"
+
+import { QUERY_KEYS } from "@/libs/constants/queryKeys"
 
 export default function TodosAboutGoalCard() {
+  // 1. 내 모든 목표 리스트 조회
+  // 2. 목표별 할 일 리스트 조회
+  // 3. 목표별 진행률 계산해서 UI 보여주기
+  // 4. 목표 무한스크롤 구현 (stale time 등 캐시 적용 고려)
+
+  const { data: goalList } = useQuery({
+    queryKey: [QUERY_KEYS.getGoalList],
+    queryFn: getGoalList,
+  })
+
+  const goalId = 2 // 특정 아이디 전달하기
+
+  const { data: todosAboutGoal } = useQuery({
+    queryKey: [QUERY_KEYS.getAllTodos, goalId],
+    queryFn: () =>
+      getAllTodos({
+        goalId: 2,
+      }),
+    enabled: !!goalList?.goals, // 특정 아이디 전달하기
+  })
+
+  console.log(todosAboutGoal)
+
   return (
     <div
       className={`
@@ -19,10 +48,11 @@ export default function TodosAboutGoalCard() {
             height={40}
           />
           <span className="text-base tablet:text-lg font-semibold text-basic">
-            최근 등록한 할 일
+            목표 별 할 일
           </span>
         </div>
       </div>
+      <div></div>
     </div>
   )
 }
--- src/pages/api/todos/getAllTodos.ts
+++ src/pages/api/todos/getAllTodos.ts
@@ -1,7 +1,25 @@
 import axiosInstance from "@/libs/axios/axiosInstance"
 import { AllTodos } from "@/types/todos"
 
-export default async function getAllTodos() {
-  const response = await axiosInstance.get<AllTodos>("/todos")
+type getAllTodosProps = {
+  [key: string]: unknown
+  goalId?: number
+  done?: boolean
+  cursor?: number
+  size?: number
+}
+
+export default async function getAllTodos(data: getAllTodosProps) {
+  const queryParams = new URLSearchParams({})
+
+  for (const params in data) {
+    if (data[params]) {
+      queryParams.append(params, (data[params] as number | boolean).toString())
+    }
+  }
+
+  const response = await axiosInstance.get<AllTodos>(
+    `/todos?${queryParams.toString()}`,
+  )
   return response.data
 }
