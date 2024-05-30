b6bfcfaa7380285350235b70d43ef8de8511eee1
ParkSohyunee <sohyunee016@gmail.com>
2024-05-30 22:04:43 +0900

    feat: 목표 리스트 조회 및 UI 구현

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image"
+import Link from "next/link"
 import { ChangeEvent, useState, KeyboardEvent } from "react"
 import { useQuery } from "@tanstack/react-query"
 
 import { QUERY_KEYS } from "@/libs/constants/queryKeys"
 import getUser from "@/pages/api/user/getUser"
-import Link from "next/link"
 import createGoal from "@/pages/api/goal/createGoal"
+import getGoalList from "@/pages/api/goal/getGoalList"
 
 export default function Sidebar() {
   const [isVisibleInput, setIsVisibleInput] = useState(false)
   const [newGoal, setNewGoal] = useState("")
+
   const { data: user } = useQuery({
     queryKey: [QUERY_KEYS.getUser],
     queryFn: getUser,
+  })
+
+  const { data } = useQuery({
+    queryKey: [QUERY_KEYS.getGoalList],
+    queryFn: getGoalList,
+    staleTime: 1000 * 60 * 5,
   })
 
   const handleVisibleInput = () => {
@@ -23,8 +31,9 @@ export default function Sidebar() {
     setNewGoal(e.target.value)
   }
 
-  // TODO 목표 생성 후, 리스트 업데이트되도록
-  // TODO 리액트 훅 폼으로 변경 고려
+  // TODO 목표 리스트 조회해서 UI 그리기
+  // TODO 목표 생성 후, 리스트 업데이트되도록 - Optimistic UI로 리팩토링 해보기
+  // TODO 리액트 훅 폼으로 변경 고려 - 리팩토링
   const onSubmit = async (e: KeyboardEvent) => {
     if (e.code === "Enter") {
       if (!newGoal) return
@@ -97,7 +106,11 @@ export default function Sidebar() {
           </div>
           <div className="flex flex-col gap-6">
             <ul className="list-disc list-inside text-sm font-medium text-slate-700">
-              <li className="p-2">가나다</li>
+              {data?.goals.map((goal) => (
+                <li key={goal.id} className="p-2">
+                  {goal.title}
+                </li>
+              ))}
               {isVisibleInput && (
                 <li className="p-2">
                   <input
