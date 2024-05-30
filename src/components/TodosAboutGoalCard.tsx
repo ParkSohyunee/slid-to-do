8409db74f4e2f7fde4b92daf842f47f8e63806ee
ParkSohyunee <sohyunee016@gmail.com>
2024-05-30 10:00:43 +0900

    design: 할 일 없을 때, 로딩중일 때 UI 구현

--- src/components/TodosAboutGoalCard.tsx
+++ src/components/TodosAboutGoalCard.tsx
@@ -29,7 +29,7 @@ export default function TodosAboutGoalCard({
    * TODO
    * - [x] 노트 아이콘 추가, hover 디자인
    * - [ ] 반응형 디자인
-   * - [ ] 할 일 없음, 로딩중 UI
+   * - [x] 할 일 없음, 로딩중 UI
    * - [ ] 더보기 버튼 클릭시 더 보여주기
    * - [ ] 새 할 일 버튼을 클릭 시, 할일 생성 페이지로 이동하고, 목표란은 해당 목표로 자동 입력
    */
@@ -59,43 +59,55 @@ export default function TodosAboutGoalCard({
         <div>프로그래스바</div>
       </div>
       <div className="flex gap-6 self-stretch">
-        {results.map(({ data }, index) => (
+        {results.map(({ data, isLoading }, index) => (
           <ul key={index} className="flex flex-col gap-3 grow-[1]">
             <p className="text-sm font-semibold text-basic">
               {index === 0 ? "To do" : "Done"}
             </p>
-            <div className="flex flex-col items-start gap-2 self-stretch">
-              {data?.todos.map((todo) => (
-                <li
-                  key={todo.id}
-                  className={`
+            {isLoading ? (
+              <p className="text-sm font-normal text-slate-500 text-center">
+                로딩중
+              </p>
+            ) : data?.todos.length !== 0 ? (
+              <div className="flex flex-col items-start gap-2 self-stretch">
+                {data?.todos.map((todo) => (
+                  <li
+                    key={todo.id}
+                    className={`
                   text-sm font-normal text-basic 
                   flex items-center justify-between gap-2 
                   ${todo.done && "line-through"}
                   `}
-                >
-                  <Image
-                    src={
-                      todo.done
-                        ? "/icons/checkbox-active.svg"
-                        : "/icons/checkbox-inactive.svg"
-                    }
-                    alt="할 일 완료 여부"
-                    width={24}
-                    height={24}
-                  />
-                  {todo.title}
-                  {todo.noteId && (
+                  >
                     <Image
-                      src="/icons/note-view.svg"
+                      src={
+                        todo.done
+                          ? "/icons/checkbox-active.svg"
+                          : "/icons/checkbox-inactive.svg"
+                      }
                       alt="할 일 완료 여부"
                       width={24}
                       height={24}
                     />
-                  )}
-                </li>
-              ))}
-            </div>
+                    {todo.title}
+                    {todo.noteId && (
+                      <Image
+                        src="/icons/note-view.svg"
+                        alt="할 일 완료 여부"
+                        width={24}
+                        height={24}
+                      />
+                    )}
+                  </li>
+                ))}
+              </div>
+            ) : (
+              <p className="text-sm font-normal text-slate-500 text-center">
+                {index === 0
+                  ? "아직 해야할 일이 없어요"
+                  : "아직 다 한 일이 없어요"}
+              </p>
+            )}
           </ul>
         ))}
       </div>
