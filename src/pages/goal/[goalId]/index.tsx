2ff28177d250793af83cea8f167e969d9a8755f6
ParkSohyunee <sohyunee016@gmail.com>
2024-06-01 13:20:41 +0900

    feat: 목표별 노트 모아보기 페이지로 이동하기 기능 추가

--- src/pages/goal/[goalId]/index.tsx
+++ src/pages/goal/[goalId]/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { useRouter } from "next/router"
+import Link from "next/link"
 
 import GoalDetailCard from "@/components/GoalDetailCard"
 
@@ -8,10 +9,10 @@ import GoalDetailCard from "@/components/GoalDetailCard"
  * [x] 목표 상세 조회 API 연동 (title)
  * [ ] 목표 ID를 가지고, 할 일, 진행률 API 연동 (progress, todo, done)
  * [x] UI 구현
- * [ ] 목표 수정, 삭제 기능 구현
+ * [x] 목표 수정, 삭제 기능 구현
  * [x] 수정, 삭제 팝업 메뉴 구현
- * [ ] 목표 수정시 사이드바에 반영
- * [ ] 목표 삭제의 경우 확인 모달 띄우기
+ * [x] 목표 수정시 사이드바에 반영
+ * [x] 목표 삭제의 경우 확인 모달 띄우기
  * [ ] 할 일 없음 UI, 로딩 UI
  * [ ] 기타 애니메이션 적용
  * [ ] 각 할 일 카드 무한스크롤 적용
@@ -25,10 +26,12 @@ export default function GoalDetailPage() {
       <h1 className="mb-4 text-lg font-semibold text-slate-900">목표</h1>
       <div className="flex flex-col gap-6 h-full">
         <GoalDetailCard goalId={goalId} />
-        <div
+        <Link
+          href={`/goal/${goalId}/notes`}
           className="
           flex items-center justify-between
           bg-blue-100 px-6 py-4 border border-bule-100 rounded-sm
+          cursor-pointer
           "
         >
           <div className="flex items-center gap-2">
@@ -40,9 +43,8 @@ export default function GoalDetailPage() {
             alt="노트 모아보기 버튼"
             width={24}
             height={24}
-            className="cursor-pointer"
           />
-        </div>
+        </Link>
         <div className="grid gap-6 grid-cols-2">
           <div
             className="
