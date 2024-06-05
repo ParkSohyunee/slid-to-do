d615cbf35af02789ab2ff1c0eabd93317bece1c3
ParkSohyunee <sohyunee016@gmail.com>
2024-06-05 23:45:40 +0900

    design: 페이지 배경 색상, 에디터 폰트, 색상 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,11 +8,17 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   const hiddenLayoutPath = ["/login", "/signup"]
+  const bgWhiteStyle = ["/todos-list/[todoId]/write-note"]
   const router = useRouter()
   const isHiddenSideBar = hiddenLayoutPath.includes(router.asPath)
+  const bgColor = bgWhiteStyle.includes(router.pathname)
+    ? "bg-white"
+    : "bg-slate-100"
 
   return (
-    <section className="flex wrapper bg-slate-100 max-mobile:flex-col min-h-screen">
+    <section
+      className={`flex wrapper ${bgColor} max-mobile:flex-col min-h-screen`}
+    >
       {!isHiddenSideBar && <Sidebar />}
       <main className={`grow ${!isHiddenSideBar && "p-4 tablet:p-6 xl:pl-20"}`}>
         {children}
