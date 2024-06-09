e757afd7435f71f5804dd9b4e4ebcc948d78514b
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-06-09 16:10:38 +0900

    ✨ feat: 회원가입 페이지 UI 및 기능 구현 (#17)
    * feat: 회원가입 페이지 UI 및 input form 구현
    
    * design: 회원가입, 로그인 페이지 레이아웃 수정
    
    * feat: 회원가입 API 연동 및 비밀번호 확인 input 유효성 검사 추가
    
    * feat: 비밀번호 보기, 가리기 기능 구현
    
    * feat: 로그인페이지에도 비밀번호 보이기, 숨기기 기능 추가
    
    * refactor: 회원가입 페이지 입력 form 공통 컴포넌트(TextField) 사용하여 리팩토링
    
    * feat: TextField Story에 비밀번호 input 스토리 추가

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,180 @@
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { FormProvider, useForm } from "react-hook-form"
+
+import {
+  emailValidationRules,
+  nameValidationRules,
+  passwordForSignUpValidationRules,
+} from "@/libs/utils/formInputValidationRules"
+import axiosInstance from "@/libs/axios/axiosInstance"
+import { AxiosError } from "axios"
+
+import Label from "@/components/Label"
+import TextField from "@/components/TextField"
+
+type SignUpFormVaules = {
+  email: string
+  name: string
+  password: string
+  passwordConfirm: string
+}
+
+export default function SignUpPage() {
+  const router = useRouter()
+  const [visiblePassword, setVisiblePassword] = useState(false)
+  const [visiblePasswordConfirm, setVisiblePasswordConfirm] = useState(false)
+  const methods = useForm<SignUpFormVaules>({ mode: "onBlur" })
+  const {
+    handleSubmit,
+    watch,
+    setError,
+    formState: { isValid, isSubmitting },
+  } = methods
+
+  const handleVisiblePassword =
+    (value: "password" | "passwordConfirm") => () => {
+      if (value === "password") {
+        setVisiblePassword((prev) => !prev)
+      } else {
+        setVisiblePasswordConfirm((prev) => !prev)
+      }
+    }
+
+  const handleSubmitForm = async (data: SignUpFormVaules) => {
+    try {
+      await axiosInstance.post("/user", {
+        email: data.email,
+        name: data.name,
+        password: data.password,
+      })
+      router.push("/login") // TODO 가입 완료 토스트메세지
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.data.message === "이미 사용 중인 이메일입니다.") {
+          setError("email", {
+            type: "custom",
+            message: error.response?.data.message,
+          })
+        }
+      }
+    }
+  }
+
+  return (
+    <section
+      className={`
+      h-full
+      flex flex-col items-center 
+      py-[120px] 
+      max-tablet:pt-[64px] max-mobile:pt-[48px] 
+      px-[52px] max-tablet:px-4
+    `}
+    >
+      <div className="mb-10">
+        <Image
+          src={"/logo/img_logo.svg"}
+          alt="logo"
+          width={270}
+          height={89}
+          priority
+        />
+      </div>
+      <FormProvider {...methods}>
+        <form
+          onSubmit={handleSubmit(handleSubmitForm)}
+          className="flex flex-col gap-12 max-w-[640px] w-full"
+        >
+          <div className="flex flex-col gap-11">
+            <div>
+              <Label htmlFor="name">이름</Label>
+              <TextField
+                field="name"
+                placeholder="이름을 입력해 주세요"
+                validationRules={nameValidationRules}
+              />
+            </div>
+            <div>
+              <Label htmlFor="email">이메일</Label>
+              <TextField
+                field="email"
+                placeholder="이메일을 입력해 주세요"
+                validationRules={emailValidationRules}
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">비밀번호</Label>
+              <TextField
+                field="password"
+                placeholder="비밀번호를 입력해 주세요"
+                validationRules={passwordForSignUpValidationRules}
+                textInputType={visiblePassword}
+              >
+                <Image
+                  onClick={handleVisiblePassword("password")}
+                  className="absolute top-1/2 right-6 transform -translate-y-1/2 cursor-pointer"
+                  src={`/icons/visibility-${visiblePassword ? "on" : "off"}.svg`}
+                  alt="비밀번호 보기"
+                  width={24}
+                  height={24}
+                />
+              </TextField>
+            </div>
+            <div>
+              <Label htmlFor="passwordConfirm">비밀번호 확인</Label>
+              <TextField
+                field="passwordConfirm"
+                placeholder="비밀번호를 다시 한 번 입력해 주세요"
+                validationRules={{
+                  required: "비밀번호를 다시 한 번 입력해 주세요.",
+                  validate: (value) => {
+                    if (watch("password") !== value) {
+                      return "비밀번호가 일치하지 않습니다."
+                    }
+                    return true
+                  },
+                }}
+                textInputType={visiblePasswordConfirm}
+              >
+                <Image
+                  onClick={handleVisiblePassword("passwordConfirm")}
+                  className="absolute top-1/2 right-6 transform -translate-y-1/2 cursor-pointer"
+                  src={`/icons/visibility-${visiblePasswordConfirm ? "on" : "off"}.svg`}
+                  alt="비밀번호 보기"
+                  width={24}
+                  height={24}
+                />
+              </TextField>
+            </div>
+          </div>
+          <div className="flex flex-col gap-10">
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className={`
+            py-3 flex 
+            justify-center items-center 
+            self-stretch rounded-sm 
+            text-white text-base font-semibold
+            outline-none
+            hover:bg-blue-600 active:bg-blue-800
+            ${isValid ? "bg-blue-500" : "bg-slate-400"}
+            transition-colors duration-500
+            `}
+            >
+              회원가입하기
+            </button>
+            <p className="text-sm font-medium flex gap-1 justify-center">
+              <span className="text-basic">이미 회원이신가요?</span>
+              <span className="text-bland-blue underline">
+                <Link href={"/login"}>로그인</Link>
+              </span>
+            </p>
+          </div>
+        </form>
+      </FormProvider>
+    </section>
+  )
+}
--- src/stories/TextField.stories.tsx
+++ src/stories/TextField.stories.tsx
@@ -1,12 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import TextField from "@/components/TextField"
 import { FormProvider, useForm } from "react-hook-form"
+import Image from "next/image"
 
 const meta = {
   title: "TextField/DefaultTextField",
   component: TextField,
   parameters: {
-    layout: "centered",
+    layout: "padded",
   },
   tags: ["autodocs"],
   decorators: [
@@ -19,6 +20,7 @@ const meta = {
   argTypes: {
     field: { control: "text", description: "input register name" },
     placeholder: { control: "text", description: "placeholder" },
+    textInputType: { control: "boolean", description: "input type" },
     validationRules: {
       control: "object",
       description: "input 유효성 검사 Rules",
@@ -40,5 +42,25 @@ export const Default: Story = {
         message: "이메일 형식으로 작성해 주세요.",
       },
     },
+  },
+}
+
+export const PasswordInput: Story = {
+  args: {
+    field: "password",
+    placeholder: "비밀번호를 입력해 주세요",
+    textInputType: false,
+    validationRules: {
+      required: "비밀번호를 입력해 주세요.",
+    },
+    children: (
+      <Image
+        className="absolute top-1/2 right-6 transform -translate-y-1/2 cursor-pointer"
+        src={`/icons/visibility-off.svg`}
+        alt="비밀번호 보기"
+        width={24}
+        height={24}
+      />
+    ),
   },
 }
