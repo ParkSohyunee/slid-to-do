9f32e427a358105ec7e3055f3c0b7b2b11418a5f
ParkSohyunee <sohyunee016@gmail.com>
2024-06-11 14:22:44 +0900

    feat: useInfiniteQuery를 사용한 무한스크롤 기능 구현

--- src/pages/goal/[goalId]/notes/index.tsx
+++ src/pages/goal/[goalId]/notes/index.tsx
@@ -1,25 +1,44 @@
 import Image from "next/image"
 import { useRouter } from "next/router"
-import { useQuery } from "@tanstack/react-query"
+import { useMemo } from "react"
+import { useInfiniteQuery } from "@tanstack/react-query"
 
 import { QUERY_KEYS } from "@/libs/constants/queryKeys"
 import getNoteList from "@/pages/api/note/getNoteList"
 import { NoteList } from "@/types/note"
 import NoteListCards from "@/components/card/NoteListCards"
+import useIntersectionObserver from "@/hooks/useIntersectionObserver"
 
 export default function NoteListAboutGoalPage() {
   const { query } = useRouter()
   const goalId = Number(query.goalId)
 
-  const { data, isLoading } = useQuery<NoteList>({
-    queryKey: [QUERY_KEYS.getNoteList, goalId],
-    queryFn: () =>
-      getNoteList({
-        goalId,
-      }),
-    enabled: !!goalId,
-    staleTime: 1000 * 60 * 5,
+  const { data, hasNextPage, isLoading, isFetching, fetchNextPage } =
+    useInfiniteQuery<NoteList>({
+      queryKey: [QUERY_KEYS.getNoteList, goalId],
+      queryFn: ({ pageParam }) =>
+        getNoteList({
+          goalId,
+          size: 2,
+          cursor: pageParam as number,
+        }),
+      getNextPageParam: ({ nextCursor }) => (nextCursor ? nextCursor : null),
+      initialPageParam: null,
+      enabled: !!goalId,
+      staleTime: 1000 * 60 * 5,
+    })
+
+  const ref = useIntersectionObserver(() => {
+    if (hasNextPage && !isFetching) {
+      fetchNextPage()
+    }
   })
+
+  const notes = useMemo(() => {
+    return data ? data?.pages.flatMap((data) => data.notes) : []
+  }, [data])
+
+  console.log(notes) // 삭제 예정
 
   return (
     <section className="h-full max-w-1200 flex flex-col gap-4">
@@ -33,15 +52,20 @@ export default function NoteListAboutGoalPage() {
           height={24}
         />
         <h3 className="text-sm font-semibold text-basic">
-          {data?.notes[0].goal.title}
+          {notes[0]?.goal.title}
         </h3>
       </div>
       {isLoading ? (
         <div className="text-sm font-normal text-slate-500 h-full flex items-center justify-center">
           로딩 중
         </div>
-      ) : data?.notes && data?.notes.length > 0 ? (
-        data?.notes.map((note) => <NoteListCards key={note.id} note={note} />)
+      ) : notes && notes.length > 0 ? (
+        <>
+          {notes.map((note) => (
+            <NoteListCards key={note.id} note={note} />
+          ))}
+          <div ref={ref} className="h-[1px]"></div>
+        </>
       ) : (
         <div className="text-sm font-normal text-slate-500 h-full flex items-center justify-center">
           아직 등록된 노트가 없어요
