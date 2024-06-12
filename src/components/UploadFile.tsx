380a33023fa7ab72a8ee2f9f07501d6ab56ed030
ParkSohyunee <sohyunee016@gmail.com>
2024-06-12 15:51:36 +0900

    feat: file url이 있을때는 파일업로드 버튼 대신 이미지 보여지도록 적용

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -4,11 +4,13 @@ import { ChangeEvent, Dispatch, SetStateAction, useRef } from "react"
 type UploadFileProps = {
   uploadFile?: File
   setUploadFile: Dispatch<SetStateAction<File | undefined>>
+  fileUrl?: string
 }
 
 export default function UploadFile({
   uploadFile,
   setUploadFile,
+  fileUrl,
 }: UploadFileProps) {
   const hiddenInputRef = useRef<HTMLInputElement>(null)
 
@@ -21,26 +23,50 @@ export default function UploadFile({
 
   return (
     <>
-      <button
-        type="button"
-        onClick={() => hiddenInputRef.current?.click()}
-        className={`
+      {fileUrl ? (
+        <div
+          className={`
+        relative h-[184px] rounded-sm bg-slate-50 
+        text-base font-normal text-slate-400 overflow-hidden
+        `}
+        >
+          <Image
+            className="absolute right-2 top-2 rotate-45 z-[1] cursor-pointer"
+            src="/icons/close-large-white.svg"
+            alt="파일 업로드 버튼"
+            width={24}
+            height={24}
+          />
+          <Image
+            className="w-full h-full object-cover rounded-sm hover:scale-125 transition-scale duration-500"
+            src={fileUrl}
+            alt="업로드 파일"
+            fill
+          />
+        </div>
+      ) : (
+        <button
+          type="button"
+          onClick={() => hiddenInputRef.current?.click()}
+          className={`
           h-[184px] rounded-sm bg-slate-50 
           text-base font-normal text-slate-400 
           flex flex-col items-center justify-center gap-2`}
-      >
-        <Image
-          src={
-            uploadFile
-              ? "/icons/file-uploaded.svg"
-              : "/icons/gray-plus-large.svg"
-          }
-          alt="파일 업로드 버튼"
-          width={24}
-          height={24}
-        />
-        {uploadFile ? uploadFile.name : "파일을 업로드해주세요"}
-      </button>
+        >
+          <Image
+            src={
+              uploadFile
+                ? "/icons/file-uploaded.svg"
+                : "/icons/gray-plus-large.svg"
+            }
+            alt="파일 업로드 버튼"
+            width={24}
+            height={24}
+          />
+          {uploadFile ? uploadFile.name : "파일을 업로드해주세요"}
+        </button>
+      )}
+
       <input
         type="file"
         onChange={handleChangeFile}
