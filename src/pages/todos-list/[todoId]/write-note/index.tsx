fea5c43294cdf2c9b8841c31516c72b501b953ec
ParkSohyunee <sohyunee016@gmail.com>
2024-06-14 15:10:37 +0900

    feat: 노트 작성하기 페이지에 FormProvider 추가 및 링크 첨부시 UI 구현

--- src/pages/todos-list/[todoId]/write-note/index.tsx
+++ src/pages/todos-list/[todoId]/write-note/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { EditorState, convertFromRaw, convertToRaw } from "draft-js"
-import { useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 import { AxiosError } from "axios"
 
 import DefaultEditor from "@/components/editor/DefaultEditor"
@@ -23,7 +23,6 @@ export default function WriteNoteForTodoPage() {
     formState: { errors, isValid },
     watch,
     handleSubmit,
-    trigger,
     setValue,
     getValues,
   } = methods
@@ -104,23 +103,24 @@ export default function WriteNoteForTodoPage() {
   }, [todoId])
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmitNote)}
-      className="h-full max-w-1200 flex flex-col bg-white"
-    >
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="text-lg font-semibold text-slate-900">노트 작성</h1>
-        <div className="flex gap-2">
-          <button
-            type="button"
-            onClick={onClickSaveContents}
-            className="text-sm font-semibold text-blue-500 py-3 px-6"
-          >
-            임시저장
-          </button>
-          <button
-            type="submit"
-            className={`
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit(onSubmitNote)}
+        className="h-full max-w-1200 flex flex-col bg-white"
+      >
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-lg font-semibold text-slate-900">노트 작성</h1>
+          <div className="flex gap-2">
+            <button
+              type="button"
+              onClick={onClickSaveContents}
+              className="text-sm font-semibold text-blue-500 py-3 px-6"
+            >
+              임시저장
+            </button>
+            <button
+              type="submit"
+              className={`
             text-sm font-semibold text-white py-3 px-6 rounded-sm 
             ${
               isValid &&
@@ -129,66 +129,83 @@ export default function WriteNoteForTodoPage() {
                 : "bg-slate-400"
             }
             `}
-          >
-            작성 완료
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col gap-6 h-full">
-        <div className="flex flex-col gap-3">
-          {goalTitle && (
-            <h3 className="flex gap-[6px] text-base font-medium text-basic items-center">
-              <Image
-                className="w-6 h-6 rounded-[6px] bg-slate-800 p-1"
-                src="/icons/flag-white.svg"
-                alt="목표"
-                width={16}
-                height={16}
-              />
-              {goalTitle}
-            </h3>
-          )}
-          <div className="flex gap-2 items-center">
-            <div className="text-xs font-medium text-slate-700 rounded-[4px] bg-slate-100 px-[3px] py-[2px]">
-              {done ? "Done" : "To do"}
-            </div>
-            <span className="text-sm font-normal text-slate-700">
-              {todoTitle}
-            </span>
+            >
+              작성 완료
+            </button>
           </div>
-          {isShowToast && (
-            <Toast
-              message="임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?"
-              onClose={handleCloseToast}
-              onClickToast={onClickGetSavedContents}
-            />
-          )}
         </div>
-        <div className="flex flex-col gap-3 grow justify-between">
-          <div className="pt-3 pb-3 border-t border-b border-slate-200 flex items-center justify-between">
-            <input
-              {...register("title", noteTitleValidationRules)}
-              placeholder="노트의 제목을 입력해주세요"
-              autoFocus
-              className={`
+        <div className="flex flex-col gap-6 h-full">
+          <div className="flex flex-col gap-3">
+            {goalTitle && (
+              <h3 className="flex gap-[6px] text-base font-medium text-basic items-center">
+                <Image
+                  className="w-6 h-6 rounded-[6px] bg-slate-800 p-1"
+                  src="/icons/flag-white.svg"
+                  alt="목표"
+                  width={16}
+                  height={16}
+                />
+                {goalTitle}
+              </h3>
+            )}
+            <div className="flex gap-2 items-center">
+              <div className="text-xs font-medium text-slate-700 rounded-[4px] bg-slate-100 px-[3px] py-[2px]">
+                {done ? "Done" : "To do"}
+              </div>
+              <span className="text-sm font-normal text-slate-700">
+                {todoTitle}
+              </span>
+            </div>
+            {isShowToast && (
+              <Toast
+                message="임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?"
+                onClose={handleCloseToast}
+                onClickToast={onClickGetSavedContents}
+              />
+            )}
+          </div>
+          <div className="flex flex-col gap-3 grow justify-between">
+            <div className="pt-3 pb-3 border-t border-b border-slate-200 flex items-center justify-between">
+              <input
+                {...register("title", noteTitleValidationRules)}
+                placeholder="노트의 제목을 입력해주세요"
+                autoFocus
+                className={`
               text-lg font-medium text-basic w-full
               placeholder:text-slate-400 outline-none
               `}
-            />
-            <div className="flex text-xs font-medium px-1 py-[2px]">
-              <span className={errors.title ? "text-error" : ""}>
-                {watch("title")?.length ? watch("title").length : 0}
-              </span>
-              <span className="text-blue-500">{"/30"}</span>
+              />
+              <div className="flex text-xs font-medium px-1 py-[2px]">
+                <span className={errors.title ? "text-error" : ""}>
+                  {watch("title")?.length ? watch("title").length : 0}
+                </span>
+                <span className="text-blue-500">{"/30"}</span>
+              </div>
             </div>
+            <p className="text-error">{errors.title && errors.title.message}</p>
+            {getValues("linkUrl") && (
+              <div className="rounded-[20px] bg-slate-200 flex items-center gap-2 py-1 px-4">
+                <Image
+                  className="bg-blue-500 rounded-full cursor-pointer p-1 w-6 h-6"
+                  src="/icons/link-imbed.svg"
+                  alt="링크 임베드 버튼"
+                  width={24}
+                  height={24}
+                />
+                <span className="text-base font-normal text-basic hover:text-blue-500 truncate">
+                  {errors.linkUrl
+                    ? "링크 형식으로 입력해 주세요"
+                    : getValues("linkUrl")}
+                </span>
+              </div>
+            )}
+            <DefaultEditor
+              setEditorState={setEditorState}
+              editorState={editorState}
+            />
           </div>
-          <p className="text-error">{errors.title && errors.title.message}</p>
-          <DefaultEditor
-            setEditorState={setEditorState}
-            editorState={editorState}
-          />
         </div>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   )
 }
