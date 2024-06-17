3521d08724be4274435200e86c23331a52d3a2a7
ParkSohyunee <sohyunee016@gmail.com>
2024-06-17 16:53:41 +0900

    feat: 대시보드 페이지 목표 조회에 무한스크롤 적용

--- src/pages/api/goal/getGoalList.ts
+++ src/pages/api/goal/getGoalList.ts
@@ -1,7 +1,21 @@
 import axiosInstance from "@/libs/axios/axiosInstance"
 import { GoalList } from "@/types/goal"
 
-export default async function getGoalList() {
-  const response = await axiosInstance.get<GoalList>("/goals")
+type GoalListProps = {
+  [key: string]: unknown
+  cursor?: number
+  size?: number
+}
+
+export default async function getGoalList(data?: GoalListProps) {
+  const queryParams = new URLSearchParams()
+
+  for (const params in data) {
+    if (data[params] !== null) {
+      queryParams.append(params, (data[params] as number).toString())
+    }
+  }
+
+  const response = await axiosInstance.get<GoalList>(`/goals?${queryParams}`)
   return response.data
 }
