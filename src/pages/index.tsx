5b00e2b9406f6183b0ec12e0a82f1c6acaabb65e
ParkSohyunee <sohyunee016@gmail.com>
2024-06-20 09:32:27 +0900

    feat: 홈페이지로 접속했을때 로그인페이지로 리다이렉션 되도록 설정 (getServerSideProps)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
+import { GetServerSideProps } from "next"
+
 export default function Home() {
-  return (
-    <main>
-      <div className="font-bold">Bold</div>
-      <div className="font-semibold">SemiBold</div>
-      <div className="font-medium">Medium</div>
-      <div className="font-normal">Normal</div>
-      <div className="font-light">Light</div>
-      <div className="text-3xl">3xl</div>
-      <div className="text-2xl">2xl</div>
-      <div className="text-xl">xl</div>
-      <div className="text-lg">lg</div>
-      <div className="text-base">base</div>
-      <div className="text-sm">sm</div>
-      <div className="text-xs">xs</div>
-    </main>
-  )
+  return null
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: "/login",
+      permanent: true,
+    },
+  }
 }
